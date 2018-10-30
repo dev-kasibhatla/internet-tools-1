cc68dadbfb4b8254472d307aec2c3a3459a5e87c
dev-kasibhatla <dev.kasibhatla@gmail.com>
2018-10-30 21:43:57 +0530

    Update app/src/main/java/kasibhatla/dev/internet_tools/DownloadFilesTask.java

--- app/src/main/java/kasibhatla/dev/internet_tools/DownloadFilesTask.java
+++ app/src/main/java/kasibhatla/dev/internet_tools/DownloadFilesTask.java
@@ -2,5 +2,24 @@ package kasibhatla.dev.internet_tools;
 
 import android.os.AsyncTask;
 
-public class DownloadFilesTask extends AsyncTask {
+
+public class DownloadFilesTask extends AsyncTask<URL, Integer, Long> {
+        int count = urls.length;
+        long totalSize = 0;
+        for (int i = 0; i < count; i++) {
+            totalSize += Downloader.downloadFile(urls[i]);
+            publishProgress((int) ((i / (float) count) * 100));
+            // Escape early if cancel() is called
+            if (isCancelled()) break;
+        }
+        return totalSize;
+    }
+
+    protected void onProgressUpdate(Integer... progress) {
+        setProgressPercent(progress[0]);
+    }
+
+    protected void onPostExecute(Long result) {
+        showDialog("Downloaded " + result + " bytes");
+    }
 }
